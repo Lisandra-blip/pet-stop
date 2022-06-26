4fed10083377f38d2e3947a60ab08b21cebf00c5
Lisandra <lisandragazaniga@gmail.com>
2022-06-26 18:51:46 -0300

    Add existing project files to Git

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'my-first-project',
+  webDir: 'dist/my-first-project',
+  bundledWebRuntime: false
+};
+
+export default config;
